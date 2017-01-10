3ab1bfec1bb2e42e5b04bd82d174592fc77b6f01
Adam Wead <amsterdamos@gmail.com>
2017-01-10 10:52:17 -0500

    Setup zsh

---
+++ scripts/zsh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# --------------------------------------------------------------------------
+# Setup zsh
+# --------------------------------------------------------------------------
+
+if test ! $(which zsh); then
+  echo "zsh isn't installed"
+  exit 1;
+fi
+
+# Install Oh My Zsh if it isn't already present
+if [[ ! -d $HOME/.oh-my-zsh/ ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
