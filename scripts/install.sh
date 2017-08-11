330b90ce7a604acc56afd316abcb60f6c22dff87
Adam Wead <amsterdamos@gmail.com>
2017-08-10 20:59:47 -0400

    Adding Moneydance and updating bootstrap

---
+++ scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# --------------------------------------------------------------------------
+# Install packages and applications on a new system
+# --------------------------------------------------------------------------
+
+# Moneydance
+# Alternatively, try brew install moneydance
+cd ~/Downloads
+wget -q https://infinitekind.com/stabledl/current/Moneydance.zip
+unzip ~/Downloads/Moneydance.zip
+mv ~/Downloads/Moneydance.zip ~/Applications
