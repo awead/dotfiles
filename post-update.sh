afeb3f223919dc7a8da15cd31a6d1959222c31bf
Adam Wead <amsterdamos@gmail.com>
2017-10-26 16:06:48 -0400

    Updates to post-update and new files

--- post-update.sh
+++ post-update.sh
@@ -8,12 +8,7 @@
 rm ~/.curlrc
 
 source $HOME/.dotfiles/scripts/brew.sh
-
-# Update dependencies, reset mariadb and postgres databases, update zsh
-source $HOME/.dotfiles/mariadb/setup.sh
-source $HOME/.dotfiles/postgres/setup.sh
 source $HOME/.dotfiles/scripts/dependencies.sh
 source $HOME/.dotfiles/scripts/zsh.sh
-
 source $HOME/.dotfiles/scripts/home.sh
 source $HOME/.dotfiles/scripts/osx.sh
