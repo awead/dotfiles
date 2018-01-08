c8494698c4eb9c2baf2bdc88b7009b94b67df9ad
Adam Wead <amsterdamos@gmail.com>
2018-01-08 10:04:46 -0500

    Updating MySQL init process

--- mysql/setup.sh
+++ mysql/setup.sh
@@ -10,7 +10,9 @@ fi
 
 killall mysqld
 rm -Rf /usr/local/var/mysql
+rm -Rf /usr/local/etc/my.cnf.d
 mkdir /usr/local/var/mysql
+mkdir /usr/local/etc/my.cnf.d
 mysqld --initialize-insecure --explicit_defaults_for_timestamp
 mysql.server start
-mysql -u root --skip-password < $HOME/.dotfiles/mysql/init
+mysql_secure_installation
