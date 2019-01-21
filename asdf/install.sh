296c00826d3310128b86b3dde7231f0d6707cced
Adam Wead <amsterdamos@gmail.com>
2019-01-21 14:13:24 -0500

    Updating asdf

--- asdf/install.sh
+++ asdf/install.sh
@@ -3,7 +3,7 @@
 # Install asdf
 # https://github.com/asdf-vm/asdf
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.3
 source $HOME/.asdf/asdf.sh
 asdf plugin-add nodejs
 asdf plugin-add ruby
