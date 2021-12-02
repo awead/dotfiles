913b708ca03d840a86e13c7e91f9a3ad7c03f0fb
Adam Wead <awead@users.noreply.github.com>
2021-12-02 09:33:00 -0500

    Pegging to Postgres 13

--- postgres/setup.sh
+++ postgres/setup.sh
@@ -10,10 +10,10 @@ fi
 
 brew services stop postgres
 rm -Rf $HOMEBREW_PREFIX/var/postgres
-brew uninstall --force postgres
-brew install postgres
+brew uninstall --force postgresql@13
+brew install postgresql@13
 mkdir -p ~/Library/LaunchAgents
-ln -sfv $HOMEBREW_PREFIX/opt/postgresql/*.plist ~/Library/LaunchAgents
+ln -sfv $HOMEBREW_PREFIX/opt/postgresql@13/*.plist ~/Library/LaunchAgents
 launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
 brew services start postgres
