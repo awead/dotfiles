f6ef38c94e19c915c79e0aaad6510961ad7a93a2
Adam Wead <awead@users.noreply.github.com>
2024-04-06 12:45:32 -0400

    Updated whichkey shortcuts for xml stuff

--- nvim/lua/config/whichkey/init.lua
+++ nvim/lua/config/whichkey/init.lua
@@ -12,7 +12,9 @@ local mappings = {
     name = "Execute",
 
     e = { ":TermExec cmd=\"cargo run %:.\"<cr>", "Execute the current file (only supports Rust)"},
-  },
+    p = { ":TermExec cmd=\"docker run --rm -v $(pwd):/home/jstorforumadm -it jasp-t pytest %:.\" name=pytest<cr>", "Run pytest in Docker"},
+    t = { ":TermExec cmd=\"./tmp/scripts/ssio2via2 %:.\" direction=float name=ssio2via2<cr>", "Transform current file" },
+   },
 
   f = {
     name = "Finding with Telescope",
