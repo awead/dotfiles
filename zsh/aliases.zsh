7bbcefd8eefbb5c0d5295dc10b5fb3f9f0551094
Adam Wead <awead@users.noreply.github.com>
2024-07-22 21:55:30 -0400

    Updating docker compose alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,7 +15,7 @@ alias tn="TERM=screen-256color-bce tmux new -s"
 alias autocop="bundle exec rubocop -P -f fi | xargs bundle exec rubocop -a"
 alias plistbuddy="/usr/libexec/PlistBuddy"
 alias k="kubectl"
-alias dc="docker-compose" # NB: this overrides /usr/bin/dc which is a calculator program
+alias dc="docker compose" # NB: this overrides /usr/bin/dc which is a calculator program
 
 # Github repos
 alias psu-libraries='cd ~/Projects/Github/psu-libraries'
