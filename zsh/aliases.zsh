ab77837c617b7f41650e2a644cee9c1929324ef0
Adam Wead <awead@users.noreply.github.com>
2024-11-22 10:22:41 -0500

    Functions and Aliases: noid for Azure names; delete cruft

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,5 @@
 # Aliases
 alias ls='ls -Gh'
-alias firefox="~/Applications/Firefox.app/Contents/MacOS/firefox-bin"
-alias be-rspec='bundle exec rspec'
-alias be-rake='bundle exec rake'
-alias be-r='bundle exec rubocop'
-alias be-ra='bundle exec rubocop -a'
 alias date-iso-8601='date -u +"%Y-%m-%dT%H:%M:%SZ"'
 alias eject-data='hdiutil eject /Volumes/Data'
 alias dotfiles='cd ~/.dotfiles'
@@ -12,37 +7,11 @@ alias tmux="TERM=screen-256color-bce tmux"
 alias tls="tmux ls"
 alias ta="tmux attach -t"
 alias tn="TERM=screen-256color-bce tmux new -s"
-alias autocop="bundle exec rubocop -P -f fi | xargs bundle exec rubocop -a"
-alias plistbuddy="/usr/libexec/PlistBuddy"
 alias k="kubectl"
 alias dc="docker compose" # NB: this overrides /usr/bin/dc which is a calculator program
 
-# Github repos
-alias psu-libraries='cd ~/Projects/Github/psu-libraries'
-alias ss='cd ~/Projects/Github/psu-libraries/scholarsphere'
-alias catalog='cd ~/Projects/Github/psu-libraries/psulib_blacklight'
-alias traject='cd ~/Projects/Github/psu-libraries/psulib_traject'
-alias metadata-listener='cd ~/Projects/Github/psu-libraries/metadata-listener'
-alias s-config='cd ~/Projects/Github/psu-libraries/scholarsphere-config'
-alias s-client='cd ~/Projects/Github/psu-libraries/scholarsphere-client'
-alias rmd='cd ~/Projects/Github/psu-stewardship/researcher-metadata'
-
-# Dev shortcuts
-alias fcrepo-start="bundle exec fcrepo_wrapper --config ~/.fcrepo_wrapper > /dev/null 2>&1 &"
-alias solr-sufia-test="bundle exec solr_wrapper --config .internal_test_app/config/solr_wrapper_test.yml > /dev/null 2>&1 &"
-alias solr-sufia-dev="bundle exec solr_wrapper --config .internal_test_app/.solr_wrapper > /dev/null 2>&1 &"
-alias solr-dev="bundle exec solr_wrapper --config .solr_wrapper > /dev/null 2>&1 &"
-alias solr-test="bundle exec solr_wrapper --config config/solr_wrapper_test.yml > /dev/null 2>&1 &"
-alias resque-pool-start="bundle exec resque-pool -E development start"
-
-# Chrome
-alias chrome="~/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-alias chrome-canary="~/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
-alias chromium="~/Applications/Chromium.app/Contents/MacOS/Chromium"
-
-# Postgres
-alias pg-start="pg_ctl -D $PG_HOME -l $PG_HOME/server.log start"
-alias pg-stop="pg_ctl -D $PG_HOME stop -s -m fast"
+# Azure
+alias az-suffix="uuidgen | tr '[:upper:]' '[:lower:]' | awk -F\- '{ print $5 }'"
 
 # Git
 # Note: Most of these are to override the zsh git plugin
--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -8,32 +8,12 @@ function bfind {
   find . $(bundle show --paths) -name "*$@*"
 }
 
-function open-st {
-  open -a "Sublime Text" "$@"
-}
-
 function open-md {
   open -a "Marked" "$@"
 }
 
 function cwd {
   echo -n `pwd` | pbcopy
-}
-
-function furl {
-  curl --user fedoraAdmin:fedoraAdmin -k "$@"
-}
-
-function fcurl {
-  curl --user fedoraAdmin:fedoraAdmin -k "$@"
-}
-
-function rubocop-changed {
-  git status --porcelain | cut -c4- | grep -v "html.erb$" | grep ".rb" | xargs bundle exec rubocop
-}
-
-function rubocop-fix-changed {
-  git diff --name-only HEAD~1 | grep -v "html.erb$" | grep ".rb" | xargs bundle exec rubocop -a
 }
 
 function rspec-changed {
@@ -54,33 +34,6 @@ function d-rspec {
   fi
 }
 
-function pdf-concat {
-  "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o "$@"
-}
-
-# Switch to MariaDB
-function maria-switch {
-  brew unlink mysql
-  brew unlink mariadb && brew link mariadb
-  source $HOME/.dotfiles/mariadb/setup.sh
-}
-
-# Switch to MySQL
-# Download mysql because it is not in the brewfile.
-# You can't have both maria and mysql installed and linked. One must be linked with the other unlinked.
-function mysql-switch {
-  brew unlink mariadb
-  brew install mysql
-  brew unlink mysql && brew link mysql
-  source $HOME/.dotfiles/mysql/setup.sh
-}
-
-# Reset Postgres
-# Completely removes postgres and reinstalls it to a default configuration
-function reset-postgres {
-  source $HOME/.dotfiles/postgres/setup.sh
-}
-
 # Kill all processes based on a grep
 # Works far more effectively than plain, old killall
 function massacre {
@@ -92,17 +45,6 @@ function rails-new {
   rails new "$@" -m $HOME/.dotfiles/rails/template.rb -T -d postgresql
 }
 
-function clean-jetties {
-  rm -Rf tmp/solr-development* \
-         tmp/solr-test* \
-         tmp/fcrepo4-test-data \
-         tmp/fcrepo4-development-data \
-         tmp/blacklight-core \
-         tmp/hydra-dev \
-         tmp/blacklight-core-test \
-         tmp/hydra-test
-}
-
 # Start a new tmux session with a given repo from the Projects directory
 function tnr() {
   REPO=`tree -f -d -L 2 -in --noreport $HOME/Projects/Github | rg -i -e "$1" | head -n 1`
@@ -111,4 +53,8 @@ function tnr() {
   else
     TERM=screen-256color-bce tmux new -s $1 -c ${REPO}
   fi
+}
+
+function noid() {
+  uuidgen | tr '[:upper:]' '[:lower:]' | awk -F\- '{ print $5 }'
 }
