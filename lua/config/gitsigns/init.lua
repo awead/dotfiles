1d15f08127c84b73ebdc0f665aaa41ea1d406d69
Adam Wead <awead@users.noreply.github.com>
2025-01-18 01:20:02 -0500

    Squashed 'neovim/config/' content from commit 664747f
    git-subtree-dir: neovim/config
    git-subtree-split: 664747f7c7c2eb81db8e86887a7c30cd56af4c66

---
+++ lua/config/gitsigns/init.lua
@@ -0,0 +1,10 @@
+require("gitsigns").setup({
+  numhl = false,
+  signs = {
+    -- add = {text = "▎"},
+    -- change = {text = "▎"},
+    -- delete = {text = "➤"},
+    -- topdelete = {text = "➤"},
+    -- changedelete = {text = "▎"},
+  },
+})
---
+++ lua/plugins.lua
@@ -0,0 +1,235 @@
+build_with_config = function(package_name)
+  return function()
+    require("config." .. package_name)
+  end
+end
+
+return {
+
+  -- TODO: Plugins I'm considering
+  --------------------------------
+  -- { "akinsho/bufferline.nvim" },
+  -- { "bcicen/vim-jfmt" },
+  -- { "airblade/vim-gitgutter" },
+  -- { "danchoi/ri.vim" },
+  -- This does git diffs really nicely
+  -- {
+  --   "sindrets/diffview.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim"
+  --   }
+  -- }
+
+  -- Things that don't work
+  -------------------------
+  --  { "vim-ruby/vim-ruby" }, -- seems to conflict with treesitter
+
+  -- Basic stuff, including a few things I brought over from vim
+  --------------------------------------------------------------
+  -- { "AndrewRadev/splitjoin.vim" },
+  -- { "elixir-editors/vim-elixir" },
+  -- { "hashivim/vim-terraform" },
+  -- { "lukas-reineke/indent-blankline.nvim" },
+  { "machakann/vim-highlightedyank" },
+  -- { "pangloss/vim-javascript"  ,
+
+  -- The Altar to Tim Pope
+  -------------------------
+  { "tpope/vim-abolish" },
+  -- { "tpope/vim-bundler" },
+  -- { "tpope/vim-endwise" },
+  -- { "tpope/vim-fugitive" },
+  -- { "tpope/vim-projectionist" },
+  -- { "tpope/vim-rails" },
+  -- { "tpope/vim-rake" },
+  -- { "tpope/vim-repeat" },
+  -- { "tpope/vim-sensible" },
+  { "tpope/vim-surround" },
+  -- { "tpope/vim-unimpaired" },
+
+  {
+    "akinsho/toggleterm.nvim",
+    cond = not vim.g.vscode,
+    version = "*",
+    config = build_with_config("toggleterm"),
+  },
+
+  -- -- Used for linting
+  -- {
+  --   "dense-analysis/ale",
+  --   config = build_with_config("ale"),
+  -- },
+
+  {
+    "folke/tokyonight.nvim",
+    cond = not vim.g.vscode,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme tokyonight-moon]])
+    end,
+  },
+
+  {
+    "folke/which-key.nvim",
+    cond = not vim.g.vscode,
+    event = "BufWinEnter",
+    config = build_with_config("whichkey")
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    cond = not vim.g.vscode,
+    config = build_with_config("gitsigns"),
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    cond = not vim.g.vscode,
+    config = function()
+      require('Comment').setup()
+    end
+  },
+
+  --
+  -- LSP for rust
+  --
+  
+  {
+    "williamboman/mason.nvim",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = build_with_config("mason"),
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    cond = not vim.g.vscode,
+    config = build_with_config("lspconfig"),
+  },
+
+  {
+    "simrat39/rust-tools.nvim",
+    cond = not vim.g.vscode,
+    config = build_with_config("rust-tools"),
+  },
+
+  -- snippets
+  {
+    -- Completion framework:
+    "hrsh7th/nvim-cmp",
+
+    -- LSP completion source:
+    "hrsh7th/cmp-nvim-lsp",
+
+    -- Useful completion sources:
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/vim-vsnip"
+  },
+
+  --
+  -- Pretty stuff...
+  --
+  {
+    "nvim-lualine/lualine.nvim",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = build_with_config("lualine"),
+  },
+
+  -- Neotest, including any language-specific packages as well as vim-test to cover the rest
+  {
+    "nvim-neotest/neotest",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-neotest/neotest-vim-test",
+      "nvim-neotest/nvim-nio",
+      "nvim-treesitter/nvim-treesitter",
+      "olimorris/neotest-rspec",
+      "vim-test/vim-test",
+    },
+    config = build_with_config("neotest"),
+  },
+
+  -- Telescope fuzzy finder and addons
+  {
+    "nvim-telescope/telescope.nvim", tag = "0.1.8",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim"
+    },
+    cmd = "Telescope",
+    config = build_with_config("telescope")
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    }
+  },
+  {
+    "nvim-telescope/telescope-frecency.nvim",
+    cond = not vim.g.vscode,
+    dependencies = { "kkharji/sqlite.lua" }
+  },
+  { 
+    "otavioschwanck/telescope-alternate",
+    cond = not vim.g.vscode,
+  },
+
+  -- NvimTree for file browsing
+  {
+    "nvim-tree/nvim-tree.lua",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = build_with_config("nvim-tree"),
+    cmd = "NvimTreeToggle"
+  },
+
+  -- Devicons
+  {
+    "nvim-tree/nvim-web-devicons",
+    cond = not vim.g.vscode,
+    config = build_with_config("icons")
+  },
+
+  -- Treesitter for syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    cond = not vim.g.vscode,
+    dependencies = {
+      "RRethy/nvim-treesitter-endwise",
+    },
+    build = ":TSUpdate",
+    config = build_with_config("treesitter")
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    config = build_with_config("autopairs")
+  },
+}
