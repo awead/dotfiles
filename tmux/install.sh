c9c84be4c71f6f4ecc0ce4cd4852631171d926b7
Adam Wead <awead@users.noreply.github.com>
2025-03-01 15:59:18 -0500

    Updating tmux install and config

--- tmux/install.sh
+++ tmux/install.sh
@@ -3,7 +3,7 @@
 rm -rf $HOME/.tmux-themepack
 git clone https://github.com/jimeh/tmux-themepack.git $HOME/.tmux-themepack
 rm -rf $HOME/.tmux/plugins
-mkdir -p $HOME/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/
-git clone https://github.com/omerxx/tmux-sessionx.git $HOME/.tmux/plugins/
+mkdir -p $HOME/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+git clone https://github.com/omerxx/tmux-sessionx.git $HOME/.tmux/plugins/tmux-sessionx
 
